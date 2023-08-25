8ff93b84ebf3f2b2285f3d32126f91a29f6b8025
Faisal Nazir <bhatfaisalfnb@gmail.com>
2023-08-25 22:53:43 +0530

    Home Route: Sample Responses

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const dotenv = require("dotenv").config();
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = process.env.PORT;
 
 // Homepage Route
 app.get('/', (req, res) => {
-  res.send('Home Route');
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Listen to server
 app.listen(port, () => {
